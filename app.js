ca195f12477faa2088cf1514cbc43ba543667abf
Anene Praise <anenepraise1@gmail.com>
2023-06-08 14:36:31 +0100

    feat: implement rooms using smartPayId & listeners for disconnect events

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const http = require("http");
@@ -8,24 +9,44 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://smartpay-websocket-production.up.railway.app/",
+    origin:
+      process.env.LOCALHOST ||
+      "https://smartpay-websocket-production.up.railway.app/",
   },
 });
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 app.use(cors());
 
 io.on("connection", (socket) => {
+  const handshake = socket.handshake;
+  const { smartPayId, platform } = handshake.query;
+  console.log("==========================================");
   console.log("A new client connected");
+  console.log(`Platform: ${platform}`);
+  console.log(`ID: ${smartPayId}`);
+  console.log("==========================================");
+
+  socket.join(smartPayId);
+
+  socket.on("disconnecting", (reason) => {
+    console.log(`Client ${smartPayId} is about to be disconnected`);
+    console.log(`Reason: ${reason}`);
+  });
+
+  socket.on("disconnect", (reason) => {
+    console.log(`Client ${smartPayId} has disconnected`);
+    console.log(`Reason: ${reason}`);
+  });
 
   socket.on("new transaction", () => {
-    socket.broadcast.emit("new transaction");
+    socket.to(smartPayId).emit("new transaction");
   });
 
   socket.on("testing", (platform) => {
-    console.log(`Test connection established on ${platform}...`);
+    console.log(`Test: Connection established on ${platform}...`);
   });
 });
 
